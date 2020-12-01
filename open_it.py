72949cca3700fb037425a0bad45ca973c914cfc6
nikitafopf <71699460+nikitafopf@users.noreply.github.com>
2020-12-01 12:17:34 +0300

    Update open_it.py
    + generation of the dungeon

--- open_it.py
+++ open_it.py
@@ -7,11 +7,11 @@ import matplotlib as mp
 loose=0
 distance_to_mouse=0
 zzz=0
-WIDTH=1000
-HEIGHT=600
+WIDTH=1500
+HEIGHT=1000
 BULLETS=[]
 hero_BULLETS=[]
-WALLS=[]
+physics_WALLS=[]
 ENEMIES=[]
 FPS=30
 RED = (255, 0, 0)
@@ -26,6 +26,19 @@ TIME=0
 rnd=0
 mouse_proect_x=0
 mouse_proect_y=0
+room_size=20
+block_size=50
+number_rooms_H=5
+number_rooms_W=5
+number_walls_H=number_rooms_H*room_size+1
+number_walls_W=number_rooms_W*room_size+1
+ROOMS=[]
+WALLS=[]
+
+for i in range(number_rooms_H):
+        ROOMS.append([0]*number_rooms_W)
+for i in range(number_walls_H):
+        WALLS.append([0]*number_walls_H)
 
 screen = pygame.display.set_mode(( WIDTH, HEIGHT))
 class Map:
@@ -121,8 +134,8 @@ class hero_Weapon:
                     b.y=hero.y
                     b.radius=4
                     b.type="shock"
-                    b.Vx=40 * np.cos(2*np.pi*i/40)
-                    b.Vy=40 * np.sin(2*np.pi*i/40)
+                    b.Vx=30 * np.cos(2*np.pi*i/40)
+                    b.Vy=30 * np.sin(2*np.pi*i/40)
                     hero_BULLETS.append(b)
                 self.time_last_shoot_shock=TIME
         if self.weapon_type == "gun":
@@ -332,6 +345,90 @@ def spawner():
             e.color=BLUE
         e.now_action="move"
         ENEMIES.append(e)
+
+def labirint_constr(i,j,steps):
+    if steps>0:
+        if ROOMS[i][j]!=1:
+            steps-=1
+            ROOMS[i][j]=1
+        rnd=randint(1,4)
+        
+        if rnd==1:
+            if i<number_rooms_H-1:
+                labirint_constr(i+1,j,steps)
+            else:
+                labirint_constr(i,j,steps)
+                
+        if rnd==2:
+            if i>0:
+                labirint_constr(i-1,j,steps)
+            else:
+                labirint_constr(i,j,steps)
+        if rnd==3:
+            if j <number_rooms_W-1:
+                labirint_constr(i,j+1,steps)
+            else:
+                labirint_constr(i,j,steps)
+        if rnd==4:
+            if j>0:
+                labirint_constr(i,j-1,steps)
+            else:
+                labirint_constr(i,j,steps)
+
+
+def create_LABIRINT():
+
+
+
+# генерация комнат
+    labirint_constr(2,2,7)
+
+# создание стен комнат
+    for i in range(number_rooms_H):
+        for j in range(number_rooms_W):
+            if ROOMS[i][j]==1:
+                room_size_x=randint(1,3)
+                room_size_y=randint(1,3)
+                for k in range(room_size_y,room_size-room_size_y):
+                    WALLS[i*room_size+k][j*room_size+room_size_x]=1
+                    WALLS[i*room_size+k][j*room_size+room_size-room_size_x]=1
+                for k in range(room_size_x,room_size-room_size_x):                
+                    WALLS[i*room_size+room_size_y][j*room_size+k]=1
+                    WALLS[i*room_size+room_size-room_size_y][j*room_size+k]=1
+#     создание путей по оси x
+    builder=-1          
+    for i in range(number_rooms_H):
+        for j in range(number_rooms_W-1):
+            if (ROOMS[i][j]==1) and (ROOMS[i][j+1]==1):
+                for k in range(room_size):
+                    if WALLS[i*room_size+room_size//2][j*room_size+room_size//2+k]==1:
+                        builder*=-1
+                        WALLS[i*room_size+room_size//2][j*room_size+room_size//2+k]=3
+                        WALLS[i*room_size+room_size//2+1][j*room_size+room_size//2+k]=3
+                    if builder==1:
+                        WALLS[i*room_size+room_size//2-1][j*room_size+room_size//2+k]=1
+                        WALLS[i*room_size+room_size//2+2][j*room_size+room_size//2+k]=1
+#     создание путей по оси y
+    builder=-1          
+    for j in range(number_rooms_H):
+        for i in range(number_rooms_W-1):
+            if (ROOMS[i][j]==1) and (ROOMS[i+1][j]==1):
+                for k in range(room_size):
+                    if WALLS[i*room_size+room_size//2+k][j*room_size+room_size//2]==1:
+                        builder*=-1
+                        WALLS[i*room_size+room_size//2+k][j*room_size+room_size//2]=3
+                        WALLS[i*room_size+room_size//2+k][j*room_size+room_size//2+1]=3
+                    if builder==1:
+                        WALLS[i*room_size+room_size//2+k][j*room_size+room_size//2-1]=1
+                        WALLS[i*room_size+room_size//2+k][j*room_size+room_size//2+2]=1
+    for i in range(number_walls_H):    
+        for j in range(number_walls_W):
+            if WALLS[i][j]==1:
+                w=Walls()
+                w.x=j*block_size-number_walls_W*block_size//2+WIDTH//2
+                w.y=i*block_size-number_walls_H*block_size//2+HEIGHT//2
+                w.size=block_size
+                physics_WALLS.append(w)
 def check_collision():
     '''проверка столкновний объектов и
        вылета за границы игрового поля'''
@@ -350,22 +447,24 @@ def check_collision():
     for e in ENEMIES[:]:
         if e.hp <= 0:
             ENEMIES.remove(e)
-    for h_b in hero_BULLETS:                        
-             if check_wall_collision(wall1.x,wall1.y,wall1.size,h_b.x,h_b.y,h_b.radius) == True:
+    for h_b in hero_BULLETS: 
+        for w in physics_WALLS:
+             if check_wall_collision(w.x,w.y,w.size,h_b.x,h_b.y,h_b.radius) == True:
                 hero_BULLETS.remove(h_b)
                     
     for b in BULLETS:  
-        if check_wall_collision(wall1.x,wall1.y,wall1.size,b.x,b.y,b.radius) == True:
-                BULLETS.remove(b)
+        for w in physics_WALLS:
+            if check_wall_collision(w.x,w.y,w.size,b.x,b.y,b.radius) == True:
+                    BULLETS.remove(b)
     for b in BULLETS:
         if distance_to(b.x,b.y,hero.x,hero.y)<b.radius+hero.radius:
             BULLETS.remove(b)
-        if inter_arena(b.x,b.y)==False:
-            if b in BULLETS:
-                BULLETS.remove(b) 
-    for h_b in hero_BULLETS:
-        if inter_arena(h_b.x,h_b.y)==False:
-            hero_BULLETS.remove(h_b)  
+#         if inter_arena(b.x,b.y)==False:
+#             if b in BULLETS:
+#                 BULLETS.remove(b) 
+#     for h_b in hero_BULLETS:
+#         if inter_arena(h_b.x,h_b.y)==False:
+#             hero_BULLETS.remove(h_b)  
         
 def draw():
     '''отрисовка всех объектов. '''
@@ -378,6 +477,8 @@ def draw():
     hero.draw()
     for h_b in hero_BULLETS:
         h_b.draw()
+    for w in physics_WALLS:
+        w.draw()
     aim.draw()
 def step():
     '''функция совершающая действия каждый тик. 
@@ -405,11 +506,13 @@ def check_wall_collision_y(w_x,w_y,w_sz,x,y,r,v):
         return True
     if (x>=w_x-r) and (x<=w_x+w_sz+r) and (y+v-r<=w_y+w_sz) and (y-r>w_y+w_sz//2):
         return True
+create_LABIRINT()
 my_map=Map()
 hero=Hero()
 hero_weapon=hero_Weapon()
 aim=Aim()
-wall1=Walls()
+w=Walls()
+physics_WALLS.append(w)
 pygame.mouse.set_visible(False)
 # тест
 TIME=0
@@ -437,10 +540,11 @@ while not finished:
             hero.proect_Vy-=1
     hero. calculate_speed()
     if hero.proect_Vx**2+hero.proect_Vy**2!=0:
-        if check_wall_collision_x(wall1.x,wall1.y,wall1.size,hero.x,hero.y,hero.radius,hero.Vx)==True:
-            hero.proect_Vx=0
-        if check_wall_collision_y(wall1.x,wall1.y,wall1.size,hero.x,hero.y,hero.radius,hero.Vy)==True:
-            hero.proect_Vy=0
+        for w in physics_WALLS:
+            if check_wall_collision_x(w.x,w.y,w.size,hero.x,hero.y,hero.radius,hero.Vx)==True:
+                hero.proect_Vx=0
+            if check_wall_collision_y(w.x,w.y,w.size,hero.x,hero.y,hero.radius,hero.Vy)==True:
+                hero.proect_Vy=0
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
@@ -477,8 +581,8 @@ while not finished:
                 
             if event.key == pygame.K_SPACE:
                 if TIME>hero.blink_cd+hero.last_use_blink:
-                    hero.x=aim.mx
-                    hero.y=aim.my
+                    hero.x=aim.mx+my_map.x
+                    hero.y=aim.my+my_map.y
                     hero.last_use_blink=TIME
 
                 
@@ -486,8 +590,6 @@ while not finished:
     hero.move()      
     my_map.change_coords()
     step()
-    wall1.draw()
     pygame.display.update()
     screen.fill(BLACK)
 pygame.quit()
-
